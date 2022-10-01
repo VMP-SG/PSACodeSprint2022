81422a7c9cb387b4ad5920a9d3875d492e045c00
Ming En <mingen8216@gmail.com>
2022-10-02 00:15:44 +0800

    update

--- modus-ponens/pages/requests/requestform.js
+++ modus-ponens/pages/requests/requestform.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { initializeApp } from "firebase/app";
-import { getStorage, ref, uploadString } from "firebase/storage";
 import submitPONData from "../../api/submitPONData";
 import Page1 from "../../components/Form/Page1";
 import Page2 from "../../components/Form/Page2";
@@ -8,51 +6,49 @@ import DashBoardHeader from "../../components/dashboard/DashboardHeader";
 import updatePONData from "../../api/updatePONData";
 
 const defaultState = {
+  status: 0,
   company: "",
   requestorFirstName: "",
   requestorLastName: "",
-  id: "",
+  requestorID: "",
   mainDescription: "",
   additionalDetails: "",
   driverFirstName: "",
   driverLastName: "",
-  passNumber: "",
+  driverPassNumber: "",
   vehicleNumber: "",
-  description_0: "",
-  quantity_0: "",
-  status: 0,
+  items: [
+    {
+      description: "",
+      quantity: "",
+      image: "",
+    }
+  ],
+  designatedOfficer: "",
+  counterSignee: "",
+  approvingAetosOfficer: "",
 };
 
 const defaultErrorState1 = {
   company: false,
   requestorFirstName: false,
   requestorLastName: false,
-  id: false,
+  requestorID: false,
   mainDescription: false,
 };
 
 const defaultErrorState2 = {
   driverFirstName: false,
   driverLastName: false,
-  passNumber: false,
+  driverPassNumber: false,
   vehicleNumber: false,
-  description_0: false,
-  quantity_0: false,
+  items: [
+    {
+      description: false,
+      quantity: false,
+    }
+  ],
 };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD7D8p_tpGw9iH1QZUFEo43ir2kR8XtR0g",
-  authDomain: "modusponens-11bff.firebaseapp.com",
-  databaseURL:
-    "https://modusponens-11bff-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "modusponens-11bff",
-  storageBucket: "modusponens-11bff.appspot.com",
-  messagingSenderId: "235677406886",
-  appId: "1:235677406886:web:3e79f1bdc3d26044f54667",
-  measurementId: "G-Q0GNNYL8R2",
-};
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
 
 const RequestForm = () => {
   const [formData, setFormData] = useState(defaultState);
